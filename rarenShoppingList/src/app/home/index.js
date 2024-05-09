bdeb6e7d15856b58468bb6c43d1482e7d218cf58
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-05-09 11:03:18 -0300

    estilizando e colocando icones

--- rarenShoppingList/src/app/home/index.js
+++ rarenShoppingList/src/app/home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, SafeAreaView, ImageBackground, 
   TextInput, TouchableOpacity } from 'react-native'
 import {Ionicons} from "@expo/vector-icons";
+import { styles } from './style';
 
 export default function Home() {
   return (
@@ -11,8 +12,24 @@ export default function Home() {
       resizeMode= 'repeat'
       style={{flex: 1, justifyContent: 'flex-start'}}>
 
-      </ImageBackground>
+      <View style={styles.header}>
+        <Text style={styles.title}> Lista de Produtos</Text>
+        <View> 
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+      </View>
 
+      {/* Lista de Produtos */}
+
+      <View style={styles.footer}>
+        <View style= {styles.inputContainer}>
+          <TextInput />
+        </View>
+        <TouchableOpacity style={styles.iconContainer}>
+         <Ionicons name="add" size={36} color= "#fff" />
+         </TouchableOpacity>
+      </View>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
