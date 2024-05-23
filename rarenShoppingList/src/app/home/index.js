4bf9114147e19f4a7e10c47829cb17c687364663
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-05-23 11:40:21 -0300

    finalização da interface

--- rarenShoppingList/src/app/home/index.js
+++ rarenShoppingList/src/app/home/index.js
@@ -1,26 +1,88 @@
-import React from 'react'
-import { View, Text, SafeAreaView, ImageBackground, 
-  TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { 
+  View,
+  Text, 
+  SafeAreaView, 
+  ImageBackground, 
+  TextInput, 
+  TouchableOpacity,
+  FlatList,
+  Alert
+} from 'react-native'
 import {Ionicons} from "@expo/vector-icons";
 import { styles } from './style';
 import ItemList from '../../components/ItemList';
+import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+  const [textInput, setTextInput] = useState('');
+  const [items, setItems] = useState([]);
+
+  const saveItemToDevice = async () => {
+
+  }
+
+  const getItemsFromDevice = async () => {
+
+  }
+
+  const addItem = () => {
+
+  }
+
+  const markItemBought = itemId => {
+
+  }
+
+  const unmarkItemBought = itemId => {
+
+  }
+
+  const removeItem = itemId => {
+
+  }
+
+  const removeAll = () => {
+    Alert.alert("Limpar Lista?", "Confirma a exclusãode todos os produtos de sua lista?",
+    [{
+      text: 'Sim', 
+      onPress: () => { setItems([])}
+    }, {
+      text: 'Cancelar',
+      style: 'cancel',
+    }]
+    )
+  }
+
   return (
     <SafeAreaView style={{ flex: 1}}>
       <ImageBackground
       source={require('../../assets/background.jpg')}
       resizeMode= 'repeat'
-      style={{flex: 1, justifyContent: 'flex-start'}}>
+      style={{flex: 1, justifyContent: 'flex-start'}}
+      >
 
       <View style={styles.header}>
         <Text style={styles.title}> Lista de Produtos</Text>
         <View> 
-          <Ionicons name="trash" size={32} color="#fff" />
+          <Ionicons name="trash" size={32} color="#fff" onPress={removeAll}/>
         </View>
       </View>
 
       {/* Lista de Produtos */}
+      <FlatList 
+        contentContainerStyle={{padding: 20, paddingBottom: 100, color: 'fff'}}      
+        data={items}
+        renderItem={({item}) =>
+        <ListItem item={item} 
+        markItem= {markItemBought}
+        unmarkItem={unmarkItemBought}
+        removeItem= {removeItem}
+        /> 
+      }
+     />
+
       <ItemList item={"Café"}></ItemList>
       <ItemList item={"Chocolate"}></ItemList>
 
@@ -31,6 +93,8 @@ export default function Home() {
           fontSize={18}
           placeholderTextColor="#aeaeae"
           placeholder="Digite o nome do produto..."
+          value={textInput}
+          onChange={(text) => setTextInput(text)}
           />
         </View>
         <TouchableOpacity style={styles.iconContainer}>
--- rarenShoppingList/src/components/ItemList/index.js
+++ rarenShoppingList/src/components/ItemList/index.js
@@ -7,8 +7,31 @@ export default function ItemList({ item, markItem, unmarkItem, removeItem }) {
   return (
     <View style={styles.itemList}>
         <View style={{ flex : 1}}> 
-            <Text style={styles.ItemToBuy}>{item}</Text>
+            <Text style={item?.bought ? styles.itemBought : styles.itemToBuy}>
+              {item?.name}
+            </Text>
         </View>
+        {!item?.bought ? (
+          <TouchableOpacity 
+          styles={styles.actionIcon}
+          onPress={() => markItem(item)}>
+            
+          <Ionicons name='bag-check-outline' size={24} color='#fff' />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity 
+          styles={styles.actionIcon}
+          onPress={() => unmarkItem(item)}>
+            
+          <Ionicons name='bag-remove-outline' size={24} color='#fff' />
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity
+          style={[styles.actionIcon, { backgroundColor:"darkred"}]}
+          onPress={() => removeItem(item)}
+          >
+            <Ionicons name='trash-bin-outline' size={24} color='#fff' />
+          </TouchableOpacity>
     </View>
   )
 }
